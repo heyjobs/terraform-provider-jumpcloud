452f63b5a4afa068d955f2e5d53d28c8d54cf859
cheelim1 <ctoh@moneylion.com>
2024-11-02 22:58:42 +0800

    Refactor resourceUserGroupAssociation to support import

--- jumpcloud/resource_user_group_association.go
+++ jumpcloud/resource_user_group_association.go
@@ -3,6 +3,7 @@ package jumpcloud
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	jcapiv2 "github.com/TheJumpCloud/jcapi-go/v2"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
@@ -11,11 +12,14 @@ import (
 
 func resourceUserGroupAssociation() *schema.Resource {
 	return &schema.Resource{
-		Description: "Provides a resource for associating a JumpCloud user group to objects like SSO applications, G Suite, Office 365, LDAP and more.",
+		Description: "Provides a resource for associating a JumpCloud user group to objects like SSO applications, G Suite, Office 365, LDAP, and more.",
 		Create:      resourceUserGroupAssociationCreate,
 		Read:        resourceUserGroupAssociationRead,
 		Update:      nil,
 		Delete:      resourceUserGroupAssociationDelete,
+		Importer: &schema.ResourceImporter{
+			State: resourceUserGroupAssociationImport,
+		},
 		Schema: map[string]*schema.Schema{
 			"group_id": {
 				Description: "The ID of the `resource_user_group` resource.",
@@ -24,13 +28,13 @@ func resourceUserGroupAssociation() *schema.Resource {
 				ForceNew:    true,
 			},
 			"object_id": {
-				Description: "The ID of the object to associate to the group.",
+				Description: "The ID of the object to associate with the group.",
 				Type:        schema.TypeString,
 				Required:    true,
 				ForceNew:    true,
 			},
 			"type": {
-				Description: "The type of the object to associate to the given group. Possible values: `active_directory`, `application`, `command`, `g_suite`, `ldap_server`, `office_365`, `policy`, `radius_server`, `system`, `system_group`.",
+				Description: "The type of the object to associate with the given group. Possible values: `active_directory`, `application`, `command`, `g_suite`, `ldap_server`, `office_365`, `policy`, `radius_server`, `system`, `system_group`.",
 				Type:        schema.TypeString,
 				Required:    true,
 				ForceNew:    true,
@@ -59,9 +63,7 @@ func resourceUserGroupAssociation() *schema.Resource {
 	}
 }
 
-func modifyUserGroupAssociation(client *jcapiv2.APIClient,
-	d *schema.ResourceData, action string) diag.Diagnostics {
-
+func modifyUserGroupAssociation(client *jcapiv2.APIClient, d *schema.ResourceData, action string) diag.Diagnostics {
 	payload := jcapiv2.UserGroupGraphManagementReq{
 		Op:    action,
 		Type_: d.Get("type").(string),
@@ -86,6 +88,10 @@ func resourceUserGroupAssociationCreate(d *schema.ResourceData, meta interface{}
 	if diags.HasError() {
 		return fmt.Errorf("Error creating user group association: %v", diags)
 	}
+
+	// Set the resource ID
+	d.SetId(fmt.Sprintf("%s/%s/%s", d.Get("group_id").(string), d.Get("object_id").(string), d.Get("type").(string)))
+
 	return resourceUserGroupAssociationRead(d, meta)
 }
 
@@ -93,27 +99,34 @@ func resourceUserGroupAssociationRead(d *schema.ResourceData, meta interface{}) 
 	config := meta.(*jcapiv2.Configuration)
 	client := jcapiv2.NewAPIClient(config)
 
+	// Retrieve the group_id, object_id, and type from the resource data
+	groupID := d.Get("group_id").(string)
+	objectID := d.Get("object_id").(string)
+	objectType := d.Get("type").(string)
+
+	// Prepare optional parameters for the API call
 	optionals := map[string]interface{}{
-		"groupId": d.Get("group_id").(string),
+		"groupId": groupID,
 		"limit":   int32(100),
 	}
 
-	graphconnect, _, err := client.UserGroupAssociationsApi.GraphUserGroupAssociationsList(
-		context.TODO(), d.Get("group_id").(string), "", "", []string{d.Get("type").(string)}, optionals)
+	// Fetch associations for the group
+	graphConnect, _, err := client.UserGroupAssociationsApi.GraphUserGroupAssociationsList(
+		context.TODO(), groupID, "", "", []string{objectType}, optionals)
 	if err != nil {
 		return err
 	}
 
-	// the ID of the specified object is buried in a complex construct
-	for _, v := range graphconnect {
-		if v.To.Id == d.Get("object_id") {
-			resourceId := d.Get("group_id").(string) + "/" + d.Get("object_id").(string)
-			d.SetId(resourceId)
+	// Check if the specified association exists
+	for _, v := range graphConnect {
+		if v.To.Id == objectID {
+			// Resource exists
+			d.SetId(fmt.Sprintf("%s/%s/%s", groupID, objectID, objectType))
 			return nil
 		}
 	}
 
-	// element does not exist; unset ID
+	// If the association does not exist, unset ID to signal resource removal
 	d.SetId("")
 	return nil
 }
@@ -121,9 +134,37 @@ func resourceUserGroupAssociationRead(d *schema.ResourceData, meta interface{}) 
 func resourceUserGroupAssociationDelete(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*jcapiv2.Configuration)
 	client := jcapiv2.NewAPIClient(config)
+
 	diags := modifyUserGroupAssociation(client, d, "remove")
 	if diags.HasError() {
 		return fmt.Errorf("Error deleting user group association: %v", diags)
 	}
 	return nil
+}
+
+func resourceUserGroupAssociationImport(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	// Expected ID format: <group_id>/<object_id>/<type>
+	idParts := strings.Split(d.Id(), "/")
+	if len(idParts) != 3 {
+		return nil, fmt.Errorf("unexpected format of ID (%s), expected <group_id>/<object_id>/<type>", d.Id())
+	}
+	groupID := idParts[0]
+	objectID := idParts[1]
+	objectType := idParts[2]
+
+	// Set the parsed values into the resource data
+	if err := d.Set("group_id", groupID); err != nil {
+		return nil, err
+	}
+	if err := d.Set("object_id", objectID); err != nil {
+		return nil, err
+	}
+	if err := d.Set("type", objectType); err != nil {
+		return nil, err
+	}
+
+	// Set the ID again to ensure consistency
+	d.SetId(fmt.Sprintf("%s/%s/%s", groupID, objectID, objectType))
+
+	return []*schema.ResourceData{d}, nil
 }
